15c325d4215f56832b841e8cfdebc6581698b1d2
z-shan <zeeshan2ahmed@gmail.com>
2017-07-25 10:36:19 -0500

    add calls for blogposts

--- src/server/controllers/blogpostController.js
+++ src/server/controllers/blogpostController.js
@@ -2,13 +2,22 @@ var blogpostController = function(BlogPost) {
     var post = function(req, res) {
         var post = new BlogPost(req.body);
 
-        post.save();
-        res.status(201);
-        res.send(post);
+        post.save(function(err, result) {
+            if(!err) {
+                res.status(201);
+                res.send(post);
+            }
+        });
     };
 
     var get = function(req, res) {
-       
+        BlogPost.find({}, function(err, posts) {
+            if(err) {
+                res.status(500).send(err);
+            } else {
+                res.send(200, posts);
+            }
+        });
     };
 
     var getById = function(req, res) {
