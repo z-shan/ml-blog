5341f79bff30786772a48958f26b6b9f03fce38d
z-shan <zeeshan2ahmed@gmail.com>
2017-07-28 13:39:06 -0500

    login ui changes

--- src/app/components/common/textInput.js
+++ src/app/components/common/textInput.js
@@ -6,8 +6,8 @@ var Input = React.createClass({
     render: function() {
         return (
             <div className="group">
-                <label htmlFor={this.props.for} className="label">{this.props.label}</label>
-                <input id={this.props.id} type="text" className="input" />
+                <label htmlFor={this.props.for} className="login-label">{this.props.label}</label>
+                <input id={this.props.id} type="text" className="login-input" />
             </div>
         );
     }
--- src/app/components/login/loginPage.js
+++ src/app/components/login/loginPage.js
@@ -6,32 +6,33 @@ var Input = require('../common/textInput');
 var LoginPage = React.createClass({
     render: function() {
         return (
-            <div className="login-wrap">
-                <div className="login-html">
-                    <input id="tab-1" type="radio" name="tab" className="sign-in" checked={this.props.selectedOption === 'signin'} value="signin" /><label htmlFor="tab-1" className="tab" value="signin" onClick={this.props.handleOptionChange.bind(null, 'signin')} >Sign In</label>
-                    <input id="tab-2" type="radio" name="tab" className="sign-up" checked={this.props.selectedOption === 'signup'} value="signup" /><label htmlFor="tab-2" className="tab" value="signup" onClick={this.props.handleOptionChange.bind(null, 'signup')} >Sign Up</label>
-                    <div className="login-form">
-                        <div className="sign-in-htm">
-                            <Input for="email" label="Email Address" id="email" value={this.props.auth.email} onChange={this.props.setAuthState}/>
-                            <Input for="password" label="Password" id="password" value={this.props.auth.password} />
+            <div className="login-wrapper">
+                <div className="login-wrap">
+                    <div className="login-html">
+                        <input id="tab-1" type="radio" name="tab" className="sign-in" checked={this.props.selectedOption === 'signin'} value="signin" /><label htmlFor="tab-1" className="tab" value="signin" onClick={this.props.handleOptionChange.bind(null, 'signin')} >Sign In</label>
+                        <input id="tab-2" type="radio" name="tab" className="sign-up" checked={this.props.selectedOption === 'signup'} value="signup" /><label htmlFor="tab-2" className="tab" value="signup" onClick={this.props.handleOptionChange.bind(null, 'signup')} >Sign Up</label>
+                        <div className="login-form">
+                            <div className="sign-in-htm">
+                                <Input for="email" label="Email Address" id="email" value={this.props.auth.email} onChange={this.props.setAuthState}/>
+                                <Input for="password" label="Password" id="password" value={this.props.auth.password} />
 
-                            <div className="group">
-                                <input type="submit" className="button" value="Sign In" />
+                                <div className="hr"></div>
+                                <div className="group">
+                                    <input type="submit" className="button" value="Sign In" />
+                                </div>
+                                
                             </div>
-                            <div className="hr"></div>
-                        </div>
-                        <div className="sign-up-htm">
-                            <Input for="name" label="Full Name" id="regname" value={this.props.register.name} onChange={this.props.setRegisterState} />
-                            <Input for="email" label="Email Address" id="regemail" value={this.props.register.name} onChange={this.props.setRegisterState}/>
-                            <Input for="password" label="Password" id="regpassword" value={this.props.register.name} onChange={this.props.setRegisterState}/>
-                            <Input for="repeatpassword" label="Repeat Password" id="regrepeatpassword" value={this.props.register.name} onChange={this.props.setRegisterState}/>
-                            
-                            <div className="group">
-                                <input type="submit" className="button" value="Sign Up" />
-                            </div>
-                            <div className="hr"></div>
-                            <div className="foot-lnk">
-                                <label for="tab-1">Already Member?</label>
+                            <div className="sign-up-htm">
+                                <Input for="name" label="Full Name" id="regname" value={this.props.register.name} onChange={this.props.setRegisterState} />
+                                <Input for="email" label="Email Address" id="regemail" value={this.props.register.name} onChange={this.props.setRegisterState}/>
+                                <Input for="password" label="Password" id="regpassword" value={this.props.register.name} onChange={this.props.setRegisterState}/>
+                                <Input for="repeatpassword" label="Repeat Password" id="regrepeatpassword" value={this.props.register.name} onChange={this.props.setRegisterState}/>
+                                
+                                <div className="hr"></div>
+                                <div className="group">
+                                    <input type="submit" className="button" value="Sign Up" />
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
