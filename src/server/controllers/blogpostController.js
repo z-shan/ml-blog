6289adbc134033b56a659a78bc627b069121ca4a
z-shan <zeeshan2ahmed@gmail.com>
2017-07-30 05:23:04 -0500

    delete blog fixed

--- src/server/controllers/blogpostController.js
+++ src/server/controllers/blogpostController.js
@@ -50,14 +50,14 @@ var blogpostController = function(BlogPost) {
     };
 
     var deletePostById = function(req, res) {
-        /*req.post.remove(function(err) {
+        req.post.remove(function(err) {
             if(err) {
                 res.status(500).send({success: false, err: err});
             } else {
                 res.status(200).send({success: true, postId: req.post._id});
             }
-        });*/
-        res.status(200).send({success: true, postId: req.post._id});
+        });
+        //res.status(200).send({success: true, postId: req.post._id});
     };
 
     var addComment = function(req, res) {
